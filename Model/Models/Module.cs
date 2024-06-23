399789d8de2a40d9dfc0b4a9cd4db9a188bfeda3
liming <lm15281858071>
2024-06-23 17:08:32 +0800

    完成身份认证实体类及实体层、服务层、仓储层创建

---
+++ Model/Models/Module.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model.Models
+{
+    /// <summary>
+    /// 接口API地址信息表
+    /// </summary>
+    public class Module
+    {
+        public int Id { get; set; }
+        /// <summary>
+        /// 父ID
+        /// </summary>
+        public int? ParentId { get; set; }
+        /// <summary>
+        /// 名称
+        /// </summary>
+        public string Name { get; set; }
+        /// <summary>
+        /// API链接地址
+        /// </summary>
+        public string LinkUrl { get; set; }
+        /// <summary>
+        /// 控制器名称
+        /// </summary>
+        public string Controller { get; set; }
+        /// <summary>
+        /// Action名称
+        /// </summary>
+        public string Action { get; set; }
+        /// <summary>
+        /// 图标
+        /// </summary>
+        public string Icon { get; set; }
+        /// <summary>
+        /// 菜单编号
+        /// </summary>
+        public string Code { get; set; }
+        /// <summary>
+        /// 排序
+        /// </summary>
+        public int OrderSort { get; set; }
+        /// <summary>
+        /// /描述
+        /// </summary>
+        public string Description { get; set; }
+        /// <summary>
+        /// 是否激活
+        /// </summary>
+        public bool Enabled { get; set; }
+    }
+}
---
+++ Model/Models/RoleModulePermission.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Reflection;
+using System.Security;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model.Models
+{
+    public class RoleModulePermission
+    {
+        public int Id { get; set; }
+        /// <summary>
+        /// 角色ID
+        /// </summary>
+        public int RoleId { get; set; }
+        /// <summary>
+        /// 菜单ID，这里就是api地址的信息
+        /// </summary>
+        public int ModuleId { get; set; }
+        /// <summary>
+        /// 按钮ID
+        /// </summary>
+        public int? PermissionId { get; set; }
+        /// <summary>
+        /// 创建时间
+        /// </summary>
+        public DateTime? CreateTime { get; set; }
+        /// <summary>
+        ///获取或设置是否禁用，逻辑上的删除，非物理删除
+        /// </summary>
+        public bool? IsDeleted { get; set; }
+    }
+}
